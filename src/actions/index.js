a4ace321ee2dfa4aa874a9bb87d4e05ae3e8164e
Robbie Greiner <robbieg@gmail.com>
2017-11-02 12:32:34 -0600

    STUCK on promise all

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,3 +15,53 @@ export const fetchHouses = () => {
       .catch(error => console.log(error));
   };
 };
+
+export const fetchMembersSuccess = (members) => {
+  return {
+    type: 'MEMBER_SUCCESS',
+    members
+  };
+};
+
+export const fetchMembers = (swornMembers) => {
+  return dispatch => {
+
+    const unresolvedPromises = swornMembers.map( memberURL =>{
+      console.log(memberURL);
+      return fetch('http://localhost:3001/api/v1/character', {
+        method: 'POST',
+        body: {url: memberURL }
+      })
+        .then(response => console.log(response));
+    });
+
+    const promiseAll = Promise.all(unresolvedPromises);
+
+    promiseAll.then( membersArray => {
+      dispatch(fetchMembersSuccess(membersArray));
+    });
+
+  };
+  // fetch initial api
+  // map through sworn members and make fetch post to characters
+  // dispatch array of members to store
+
+};
+
+// export const fetchHouses = () => {
+//   return dispatch => {
+//     fetch(`http://localhost:3001/api/v1/houses`)
+//       .then(response => response.json())
+//       .then(houses => {
+//         const unresolvedPromises: houses.map( house =>{
+//           return fetch('api/v1/character',
+//           {
+//             method: 'POST',
+//             body: {url: house.}
+//           })
+//         })
+//       })
+//       .then(results => dispatch(fetchHousesSuccess(results)))
+//       .catch(error => console.log(error));
+//   };
+// };
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,30 +3,35 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './App/App.css';
 
-const Card = ({ house }) => {
+//on click of Card
+// fetch data
+// set in store
+// if store.members render members instead of cards
+
+const Card = ({ house, getHouseMembers }) => {
   return (
-    <Link to={'/'}>
-      <div className='card'>
-        <h1>{house.name}</h1>
-        <h2>Founded: {house.founded}</h2>
-        <h3>Seats: {house.seats.map( seat => {
-          return seat;
-        })}</h3>
-        <h3>Titles: {house.titles.map( title => {
-          return title;
-        })}</h3>
-        <h4>Coat of Arms: {house.coatOfArms}</h4>
-        <h4>Ancestral Weapons: {house.ancestralWeapons.map( weapon => {
-          return weapon;
-        })}</h4>
-        <h1>{house.words}</h1>
-      </div>
-    </Link>
+    // on click getMembers and pass house.swornMembers
+    <div className='card' onClick={() => getHouseMembers(house.swornMembers)}>
+      <h1>{house.name}</h1>
+      <h2>Founded: {house.founded}</h2>
+      <h3>Seats: {house.seats.map( seat => {
+        return seat;
+      })}</h3>
+      <h3>Titles: {house.titles.map( title => {
+        return title;
+      })}</h3>
+      <h4>Coat of Arms: {house.coatOfArms}</h4>
+      <h4>Ancestral Weapons: {house.ancestralWeapons.map( weapon => {
+        return weapon;
+      })}</h4>
+      <h1>{house.words}</h1>
+    </div>
   );
 };
 
 Card.propTypes = {
-  house: PropTypes.object
+  house: PropTypes.object,
+  getHouseMembers: PropTypes.func
 };
 
 export default Card;
