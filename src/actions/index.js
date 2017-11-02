09f41d5cc5f6c9ac75390482c66173947f969519
Robbie Greiner <robbieg@gmail.com>
2017-11-02 09:25:52 -0600

    created fetch actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
 export const fakeAction = () => ({type: 'FAKE'});
+
+export const fetchHousesSuccess = houses => {
+  return {
+    type: 'HOUSES_SUCCESS',
+    houses
+  };
+};
+
+export const fetchHouses = () => {
+  return dispatch => {
+    fetch(`http://localhost:3001/api/v1/houses`)
+      .then(response => response.json())
+      .then(results => dispatch(fetchHousesSuccess(results)))
+      .catch(error => console.log(error));
+  };
+};
