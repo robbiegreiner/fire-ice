62897abfa866a79d01b9acbe029696e62fed6da0
Robbie Greiner <robbieg@gmail.com>
2017-11-02 11:31:27 -0600

    actions and reducers being tested

---
+++ src/actions/actions.test.js
@@ -0,0 +1,22 @@
+import * as actions from './index.js';
+
+describe('actions', () => {
+  it('should create an action to detect if house fetch succeeded', () => {
+    const mockData = [{
+      name: 'sure',
+      founded: 'why not',
+      seats: ['ok', 'cool'],
+      titles: ['ok', 'cool'],
+      coatOfArms: 'cool dude',
+      ancestralWeapons: ['ok', 'cool'],
+      words: ['whatever']
+    }];
+
+    const expectedAction = {
+      type: 'HOUSES_SUCCESS',
+      houses: mockData
+    };
+
+    expect(actions.fetchHousesSuccess(mockData)).toEqual(expectedAction);
+  });
+})
